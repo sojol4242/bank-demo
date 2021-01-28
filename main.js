15c3e4114515a96369c3edc2d0fb16c59e354be7
sojol4242 <bsmrstueee90@gmail.com>
2021-01-28 22:47:50 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+// const loginId = document.getElementById('loginbtnId');
+// loginId.addEventListener('click', function() {
+//     const loginContainer = document.getElementById('loginId');
+//     loginContainer.style.display = 'none';
+//     const dashContainer = document.getElementById('dashboardId');
+//     dashContainer.style.display = 'block';
+// });
